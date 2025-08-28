5f707f966947c85ed016cfb1755690a509702184
pedroServfaz <pedro.servfaz@gmail.com>
2025-08-28 16:21:15 -0300

    Geração dos campos de dias limitados a 60 dias

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,12 +1,14 @@
 import { View, Text } from 'react-native';
 import { Header } from '../components/Header';
 import { HabitDay, DAY_SIZE } from '../components/HabitDay';
+import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+const datesFromYearStart = generateRangeDatesFromYearStart();
 
 export function Home() {
   return (
-    <View className="flex-1 bg-background px-8 pt-16">
+    <View className="flex-1 bg-background px-4 pt-8">
       <Header />
 
       <View className="mb-2 mt-6 flex-row">
@@ -15,18 +17,22 @@ export function Home() {
             key={`${weekDay}-${i}`}
             style={{
               width: DAY_SIZE,
-              color: '#a1a1aa', // zinc-400
-              fontSize: 20, // text-xl
+              color: '#a1a1aa',
+              fontSize: 20,
               fontWeight: 'bold',
               textAlign: 'center',
-              margin: 4, // mesmo m-1 do HabitDay (m-1 = 4px)
+              margin: 4,
             }}>
             {weekDay}
           </Text>
         ))}
       </View>
 
-      <HabitDay />
+      <View className="flex-row flex-wrap">
+        {datesFromYearStart.slice(0, 60).map((date) => (
+          <HabitDay key={date.toISOString()} />
+        ))}
+      </View>
     </View>
   );
 }
