f20c86156fa31b55813cb1d4382b6e0c2c935995
pedroServfaz <pedro.servfaz@gmail.com>
2025-09-02 16:26:42 -0300

    Navegação para a tela de cadastro

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,16 @@ import { View, TouchableOpacity, Text } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import Logo from '../assets/logo.svg';
 import colors from 'tailwindcss/colors';
+import { useNavigation } from '@react-navigation/native';
 
 export function Header() {
+  const { navigate } = useNavigation();
   return (
     <View className="mt-14 w-full flex-row items-center justify-between px-2">
       <Logo />
       <TouchableOpacity
         activeOpacity={0.7}
+        onPress={() => navigate('new')}
         className="h-11 flex-row items-center rounded-lg border border-violet-500 px-4">
         <Feather name="plus" color={colors.violet[500]} size={20} />
         <Text className="ml-3 font-semibold text-base text-white">Novo</Text>
