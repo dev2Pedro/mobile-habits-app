cfe6bbcc61474fce76d537ad2c09434f9d8e0595
pedroServfaz <pedro.servfaz@gmail.com>
2025-09-04 17:23:15 -0300

    BackButton no habit

--- screens/Habit.tsx
+++ screens/Habit.tsx
@@ -1,5 +1,24 @@
-import { View } from 'react-native';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import { BackButton } from 'components/BackButton';
+
+interface Params {
+  date: string;
+}
 
 export function Habit() {
-  return <View className="flex-1 bg-background px-8 pt-16"></View>;
+  const route = useRoute();
+  const { date } = route.params as Params;
+
+  console.log(date);
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#1E1E1E', paddingHorizontal: 32 }}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingTop: 18, paddingBottom: 100 }}>
+        <BackButton />
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
